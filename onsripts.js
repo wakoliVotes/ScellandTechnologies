ab2ec90b4535d01aae6035adb4477494345f8b5e
wakolivotes <votesdw@yahoo.com>
2023-10-27 20:56:54 +0300

    27

---
+++ onsripts.js
@@ -0,0 +1,14 @@
+const OnScriptsItems = () => {
+    const navbarItems = document.querySelector('.navbar-items');
+    const footerItems = document.querySelector('.footer-items');
+    const subscribe = document.querySelector('.subscribe');
+    const relatedBlogs = document.querySelector('.related-blogs');
+
+    navbarItems.innerHTML = ``;
+    footerItems.innerHTML = ``;
+    subscribe.innerHTML = ``;
+    relatedBlogs.innerHTML = ``;
+
+
+}
+OnScriptsItems();
