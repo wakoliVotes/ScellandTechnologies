8b6b348e84dd11799c579ff9ae921bd9df4f7af7
wakoiVotes <votesdw@yahoo.com>
2024-02-07 09:56:28 +0300

    Date 7th, 2024

--- onsripts.js
+++ onsripts.js
@@ -1,12 +1,176 @@
 const OnScriptsItems = () => {
-    const navbarItems = document.querySelector('.navbar-items');
-    const footerItems = document.querySelector('.footer-items');
-    const subscribe = document.querySelector('.subscribe');
+    const navbarItems = document.querySelector('.navbar');
+    const footerItems = document.querySelector('.footer');
+    const signUpNow = document.querySelector('.cta-section');
     const relatedBlogs = document.querySelector('.related-blogs');
 
-    navbarItems.innerHTML = ``;
-    footerItems.innerHTML = ``;
-    subscribe.innerHTML = ``;
+    navbarItems.innerHTML = `
+    <a href="/" aria-current="page" class="brand w-nav-brand w--current"
+    ><img
+      src="https://uploads-ssl.webflow.com/63201b51f3e2d01beecbb8b7/632a9c94d018c2559bf7a584_ecolabs-logo.svg"
+      alt="Ecolabs"
+  /></a>
+  <nav role="navigation" class="nav-menu w-nav-menu">
+    <a
+      href="/"
+      aria-current="page"
+      class="nav-link w-nav-link w--current"
+      >Home</a
+    ><a href="/about" class="nav-link w-nav-link">About</a
+    ><a href="/courses" class="nav-link w-nav-link">Courses</a
+    ><a href="/teachers" class="nav-link w-nav-link">Techers</a
+    ><a href="/blog" class="nav-link w-nav-link">Blog</a>
+    <div class="header-button">
+      <a href="/contact" class="outline-button nav-button w-button"
+        >Contact</a
+      >
+    </div>
+  </nav>
+  <div class="menu-button w-nav-button">
+    <div class="top-line"></div>
+    <div class="middle-line"></div>
+    <div class="bottom-line"></div>
+  </div>
+    `;
+
+    footerItems.innerHTML = `
+    <div class="container w-container">
+    <div class="footer-top">
+      <div class="ft-left">
+        <a href="/" aria-current="page" class="w-inline-block w--current"
+          ><img
+            src="https://uploads-ssl.webflow.com/63201b51f3e2d01beecbb8b7/6343f5a7b7c5def7d3d5fca0_footer-logo.svg"
+            loading="lazy"
+            alt="Ecolabs"
+        /></a>
+        <div class="footer-menu">
+          <a
+            href="/"
+            aria-current="page"
+            class="footer-link f-first-link w--current"
+            >Home</a
+          ><a href="/about" class="footer-link">About</a
+          ><a href="/courses" class="footer-link">Courses</a
+          ><a href="/teachers" class="footer-link">Teachers</a
+          ><a href="/blog" class="footer-link">Blog</a
+          ><a href="/contact" class="footer-link">Contact</a>
+        </div>
+      </div>
+      <div class="ft-right">
+        <a
+          href="https://twitter.com/"
+          target="_blank"
+          class="social-link w-inline-block"
+          ><img
+            src="https://uploads-ssl.webflow.com/63201b51f3e2d01beecbb8b7/6343f888dbb1a122e12f5f4e_twitter.svg"
+            loading="lazy"
+            alt="Icon" /></a
+        ><a
+          href="http://facebook.com/"
+          target="_blank"
+          class="social-link w-inline-block"
+          ><img
+            src="https://uploads-ssl.webflow.com/63201b51f3e2d01beecbb8b7/6343f89d8116cd597b52bcb7_facebook.svg"
+            loading="lazy"
+            alt="Icon" /></a
+        ><a
+          href="http://youtube.com/"
+          target="_blank"
+          class="social-link w-inline-block"
+          ><img
+            src="https://uploads-ssl.webflow.com/63201b51f3e2d01beecbb8b7/6343f8ab8d435cab6978946d_youtube.svg"
+            loading="lazy"
+            alt="Icon" /></a
+        ><a
+          href="http://instagram.com/"
+          target="_blank"
+          class="social-link w-inline-block"
+          ><img
+            src="https://uploads-ssl.webflow.com/63201b51f3e2d01beecbb8b7/6343f8b5feeda34757e88104_instagram.svg"
+            loading="lazy"
+            alt="Icon" /></a
+        ><a
+          href="http://linkedin.com/"
+          target="_blank"
+          class="social-link w-inline-block"
+          ><img
+            src="https://uploads-ssl.webflow.com/63201b51f3e2d01beecbb8b7/6343f8c2feeda3b5c9e89cc7_linkedin.svg"
+            loading="lazy"
+            alt="Icon"
+        /></a>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <div class="fb-left">
+        <div class="copyright-text">
+          Designed by
+          <a href="https://nixar.io/" target="_blank" class="copyright-link"
+            >Nixar</a
+          >. Powered by
+          <a
+            href="https://webflow.com/"
+            target="_blank"
+            class="copyright-link"
+            >Webflow</a
+          >.
+        </div>
+      </div>
+      <div class="fb-right">
+        <div class="fb-link">
+          <a href="/style-guide" class="footer-link f-first-link"
+            >Style Guide</a
+          ><a href="/licensing" class="footer-link">Licensing</a
+          ><a href="/changelog" class="footer-link">Changelog</a>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+    signUpNow.innerHTML = `
+    <div class="container w-container">
+    <div class="cta-wrap">
+      <h2>Still thinking about it?</h2>
+      <div class="body-small sub-heading">
+        Sign up for our newsletter and get 10% off your next course.
+      </div>
+      <div class="cta-newsletter">
+        <div class="w-form">
+          <form
+            id="wf-form-CTA-Form"
+            name="wf-form-CTA-Form"
+            data-name="CTA Form"
+            method="get"
+            class="cta-form"
+          >
+            <input
+              type="email"
+              class="cta-input w-input"
+              maxlength="256"
+              name="User-Email"
+              data-name="User Email"
+              placeholder="Enter your email here"
+              id="user-email"
+              required=""
+            /><input
+              type="submit"
+              value="Sign up Now"
+              data-wait="Please wait..."
+              class="secondary-button cta-button w-button"
+            />
+          </form>
+          <div class="success-message w-form-done">
+            <div>Thank you! Your submission has been received!</div>
+          </div>
+          <div class="error-message w-form-fail">
+            <div>
+              Oops! Something went wrong while submitting the form.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
     relatedBlogs.innerHTML = ``;
 
 
